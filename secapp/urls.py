de3dbe3032762da34cff016c84e78c422630761d
test_seccgeo <hamdi.secilmis@gmail.com>
2025-08-10 10:26:51 +0300

    refactor: create urls in secapp

--- secapp/urls.py
+++ secapp/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name="starting-page"),  # Define the index view
+    path("test", views.index, name="starting-page "),  # Define the index view
 ]
