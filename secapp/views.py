c4d6fb8ba872a4e632dbd5c6a10ea5786385d5c1
test_seccgeo <hamdi.secilmis@gmail.com>
2025-08-10 11:12:09 +0300

    refactor: create contact and urls.py and views.py in secapp

--- secapp/views.py
+++ secapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'secapp/index.html')
+
+def contact(request):
+    return render(request, 'secapp/contact.html')
